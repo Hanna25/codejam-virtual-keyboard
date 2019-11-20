9d766082911fb89fcdb4f45b762affb1dda3674d
Anna <a.sobolenko1194@gmail.com>
2019-11-20 15:53:38 +0300

    feat: keyboard switch implemented

--- index.js
+++ index.js
@@ -78,7 +78,145 @@ let buttons = [
                   {eventCode: "ControlRight", creator: createDecorativeKey, firstValue: "Ctrl", class: "controlright"}  
                 ]     
               ]
+let buttonsRu = [
+                  [
+                    {eventCode: "Backquote", creator: createSymbolKey,  firstValue: "ё", secondValue: "Ё"},
+                    {eventCode: "Digit1", creator: createSymbolKey,  firstValue: "1", secondValue: "!"},
+                    {eventCode: "Digit2", creator: createSymbolKey,  firstValue: "2", secondValue: "\""},
+                    {eventCode: "Digit3", creator: createSymbolKey,  firstValue: "3", secondValue: "№"},
+                    {eventCode: "Digit4", creator: createSymbolKey,  firstValue: "4", secondValue: ";"},
+                    {eventCode: "Digit5", creator: createSymbolKey,  firstValue: "5", secondValue: "%"},
+                    {eventCode: "Digit6", creator: createSymbolKey,  firstValue: "6", secondValue: ":"},
+                    {eventCode: "Digit7", creator: createSymbolKey,  firstValue: "7", secondValue: "?"},
+                    {eventCode: "Digit8", creator: createSymbolKey,  firstValue: "8", secondValue: "*"},
+                    {eventCode: "Digit9", creator: createSymbolKey,  firstValue: "9", secondValue: "("},
+                    {eventCode: "Digit0", creator: createSymbolKey,  firstValue: "0", secondValue: ")"},
+                    {eventCode: "Minus", creator: createSymbolKey,  firstValue: "-", secondValue: "_"},
+                    {eventCode: "Equal ", creator: createSymbolKey,  firstValue: "=", secondValue: "+"},
+                    {eventCode: "Backspace", creator: createBackspase, firstValue: "Backspace"}
+                  ],
+                  [
+                    {eventCode: "Tab", creator: createTab, firstValue: "Tab"},
+                    {eventCode: "KeyQ", creator: createSymbolKey,  firstValue: "й", secondValue: "Й"},
+                    {eventCode: "KeyW", creator: createSymbolKey,  firstValue: "ц", secondValue: "Ц"},
+                    {eventCode: "KeyE", creator: createSymbolKey,  firstValue: "у", secondValue: "У"},
+                    {eventCode: "KeyR", creator: createSymbolKey,  firstValue: "к", secondValue: "К"},
+                    {eventCode: "KeyT", creator: createSymbolKey,  firstValue: "е", secondValue: "Е"},
+                    {eventCode: "KeyY", creator: createSymbolKey,  firstValue: "н", secondValue: "Н"},
+                    {eventCode: "KeyU", creator: createSymbolKey,  firstValue: "г", secondValue: "Г"},
+                    {eventCode: "KeyI", creator: createSymbolKey,  firstValue: "ш", secondValue: "Ш"},
+                    {eventCode: "KeyO", creator: createSymbolKey,  firstValue: "щ", secondValue: "Щ"},
+                    {eventCode: "KeyP", creator: createSymbolKey,  firstValue: "з", secondValue: "З"},
+                    {eventCode: "BracketLeft", creator: createSymbolKey,  firstValue: "х", secondValue: "Х"},
+                    {eventCode: "BracketRight", creator: createSymbolKey,  firstValue: "ъ", secondValue: "Ъ"},
+                    {eventCode: "Backslash ", creator: createSymbolKey,  firstValue: "\\", secondValue: "|"},
+                    {eventCode: "Delete ", creator: createDelete, firstValue: "Del"}
+                  ],
+                  [
+                  {eventCode: "CapsLock ", creator: createCapsLock, firstValue: "Caps Lock"},
+                  {eventCode: "KeyA", creator: createSymbolKey,  firstValue: "ф", secondValue: "Ф"},
+                  {eventCode: "KeyS", creator: createSymbolKey,  firstValue: "ы", secondValue: "Ы"},
+                  {eventCode: "KeyD", creator: createSymbolKey,  firstValue: "в", secondValue: "В"},
+                  {eventCode: "KeyF", creator: createSymbolKey,  firstValue: "а", secondValue: "А"},
+                  {eventCode: "KeyG", creator: createSymbolKey,  firstValue: "п", secondValue: "П"},
+                  {eventCode: "KeyH", creator: createSymbolKey,  firstValue: "р", secondValue: "Р"},
+                  {eventCode: "KeyJ", creator: createSymbolKey,  firstValue: "о", secondValue: "О"},
+                  {eventCode: "KeyK", creator: createSymbolKey,  firstValue: "л", secondValue: "Л"},
+                  {eventCode: "KeyL", creator: createSymbolKey,  firstValue: "д", secondValue: "Д"},
+                  {eventCode: "Semicolon ", creator: createSymbolKey,  firstValue: "ж", secondValue: "Ж"},
+                  {eventCode: "Quote ", creator: createSymbolKey,  firstValue: "э", secondValue: "Э"},
+                  {eventCode: "Enter ", creator:  createEnter, firstValue: "Enter"}
+                ],
+                [
+                  {eventCode: "ShiftLeft", creator: createDecorativeKey,  firstValue: "Shift", class: " shiftleft"},
+                  {eventCode: "KeyZ", creator: createSymbolKey,  firstValue: "я", secondValue: "Я"},
+                  {eventCode: "KeyX", creator: createSymbolKey,  firstValue: "ч", secondValue: "Ч"},
+                  {eventCode: "KeyC", creator: createSymbolKey,  firstValue: "с", secondValue: "С"},
+                  {eventCode: "KeyV", creator: createSymbolKey,  firstValue: "м", secondValue: "М"},
+                  {eventCode: "KeyB", creator: createSymbolKey,  firstValue: "и", secondValue: "И"},
+                  {eventCode: "KeyN", creator: createSymbolKey,  firstValue: "т", secondValue: "Т"},
+                  {eventCode: "KeyM", creator: createSymbolKey,  firstValue: "ь", secondValue: "Ь"},
+                  {eventCode: "Comma", creator: createSymbolKey,  firstValue: "б", secondValue: "Б"},
+                  {eventCode: "Period", creator: createSymbolKey,  firstValue: "ю", secondValue: "Ю"},
+                  {eventCode: "Slash", creator: createSymbolKey,  firstValue: ".", secondValue: ","},
+                  {eventCode: "ArrowUp", creator: createUpArrow,  firstValue: "▲", class: "arrow"},
+                  {eventCode: "ShiftRight", creator: createDecorativeKey,  firstValue: "Shift", class: "shiftright"},
+                ],
+                [
+                  {eventCode: "ControlLeft", creator: createDecorativeKey,  firstValue: "Ctrl", class: "controlleft"},
+                  {eventCode: "MetaLeft ", creator: createDecorativeKey,  firstValue: "Win ", class: "win"},
+                  {eventCode: "AltLeft", creator: createDecorativeKey,  firstValue: "Alt", class: "altleft"},
+                  {eventCode: "Space", creator: createSpace},
+                  {eventCode: "AltLeft", creator: createDecorativeKey,  firstValue: "Alt", class: "altright"},
+                  {eventCode: "ArrowLeft", creator: createUpLeft,  firstValue: "◄", class: "arrow"},
+                  {eventCode: "ArrowDown", creator: createArrowDown,  firstValue: "▼", class: "arrow"},
+                  {eventCode: "ArrowRight", creator: createUpRight,  firstValue: "►", class: "arrow"},
+                  {eventCode: "ControlRight", creator: createDecorativeKey, firstValue: "Ctrl", class: "controlright" }  
+                ]  
+              ]
 
+let keydownFunctions = {
+                          "ShiftRight": doDownShiftLogic,
+                          "ShiftLeft": doDownShiftLogic,
+                          "AltRight": doDownAltLogic,
+                          "AltLeft": doDownAltLogic,
+                          "ControlRight": doDownCtrlLogic,
+                          "ControlLeft": doDownCtrlLogic,
+                       }
+let keyupFunctions = {
+                        "ShiftRight": doUpShiftLogic,
+                        "ShiftLeft": doUpShiftLogic,
+                        "AltRight": doUpAltLogic,
+                        "AltLeft": doUpAltLogic,
+                        "ControlRight": doUpCtrlLogic,
+                        "ControlLeft": doUpCtrlLogic,
+                     }
+document.addEventListener('keydown', function(event) {
+  console.log(event.code)
+  if (keydownFunctions.hasOwnProperty(event.code)) {
+    keydownFunctions[event.code]();
+  }  
+});
+document.addEventListener('keyup', function(event) {
+  console.log(event.code)
+  if (keyupFunctions.hasOwnProperty(event.code)) {
+    keyupFunctions[event.code]();
+  }  
+});
+let statusKeyboard = false;
+let statusSwitching = false;
+
+function toggleKeybord() {
+  en = document.getElementById('en-keyboard');
+  ru = document.getElementById('ru-keyboard');
+  if (en.classList.contains("off")) {
+    en.classList.remove("off");
+    ru.classList.add("off");
+  } else {
+    en.classList.add("off");
+    ru.classList.remove("off");
+  } 
+}
+function doDownShiftLogic() {
+  if (statusSwitching) {
+    toggleKeybord()
+  }
+}
+function doDownAltLogic() {
+  statusSwitching = true;
+}
+function doDownCtrlLogic() {
+  statusSwitching = true;
+}
+function doUpShiftLogic() {
+  statusSwitching = false;
+}
+function doUpAltLogic() {
+  statusSwitching = false;
+}
+function doUpCtrlLogic() {
+  statusSwitching = false;
+}
 function createKey(key) {
   let button = document.createElement('div');
   button.className = 'button';
@@ -95,7 +233,7 @@ function createSymbolKey(key) {
   span.className = 'off'
   symbolKey.appendChild(span);
   symbolKey.addEventListener('click', function(event) {
-    textarea.value = textarea.value + event.srcElement.textContent;
+    textarea.value = textarea.value + event.target.innerText;
   });
   return symbolKey;
 }
@@ -170,18 +308,26 @@ function createUpRight (key) {
   upRight.className += " " + key.class;
   return upRight;
 }
-let wrapper = document.createElement('div');
-wrapper.className = "wrapper";
-document.body.appendChild(wrapper);
-buttons.forEach(function(item) {
-  let row = document.createElement('div');
-  row.className = 'row';
-  wrapper.appendChild(row);
-  item.forEach(function(key) {
-    let button = key.creator(key);
-    row.appendChild(button);
+
+function createKeyboard(id, buttons, additionalClass) {
+  let wrapper = document.createElement('div');
+  wrapper.className = "wrapper" + additionalClass;
+  document.body.appendChild(wrapper);
+  wrapper.id = id;
+  buttons.forEach(function(item) {
+    let row = document.createElement('div');
+    row.className = 'row';
+    wrapper.appendChild(row);
+    item.forEach(function(key) {
+      let button = key.creator(key);
+      row.appendChild(button);
+    });
   });
-})
+}
+createKeyboard("en-keyboard", buttons, " off")
+createKeyboard("ru-keyboard", buttonsRu, "")
+
+
 
 
 
